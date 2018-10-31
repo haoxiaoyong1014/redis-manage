dc32c80014353cd9fb822e652041cb14f1666c9a
haoxiaoyong1014 <hxyHelloWorld@163.com>
2018-10-31 20:19:02 +0800

    add README.md

---
+++ src/main/java/haoxy/redis/manage/RedisApp.java
@@ -0,0 +1,17 @@
+package haoxy.redis.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by haoxy on 2018/10/31.
+ * E-mail:hxyHelloWorld@163.com
+ * github:https://github.com/haoxiaoyong1014
+ */
+@SpringBootApplication
+public class RedisApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApp.class,args);
+    }
+}
---
+++ src/main/java/haoxy/redis/manage/config/WebConfig.java
@@ -0,0 +1,11 @@
+package haoxy.redis.manage.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by haoxy on 2018/10/31.
+ * E-mail:hxyHelloWorld@163.com
+ * github:https://github.com/haoxiaoyong1014
+ */
+public class WebConfig {
+}
